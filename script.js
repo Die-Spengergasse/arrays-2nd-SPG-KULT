d785a708b6fe7db72de24675cb3ebf8ea27e5a9d
Tim Kulha <kul210328@spengergasse.at>
2023-12-04 22:34:45 +0100

    Großteils fertig, paar Dinge gehen noch nicht

--- script.js
+++ script.js
@@ -11,12 +11,150 @@ const button_push = document.getElementById("button_push");
 button_push.addEventListener("click", push);
 
 const button_indexof = document.getElementById("button_indexof");
+button_indexof.addEventListener("click", indexof);
+
 const button_includes = document.getElementById("button_includes");
+button_includes.addEventListener("click", includes);
+
 const button_slice = document.getElementById("button_slice");
+button_slice.addEventListener("click", slice);
+
+const button_splice = document.getElementById("button_splice");
+button_splice.addEventListener("click", splice);
+
+const button_of = document.getElementById("button_of");
+button_of.addEventListener("click", Of);
+
+const button_map = document.getElementById("button_map");
+button_map.addEventListener("click", Map);
+
+const button_filter = document.getElementById("button_filter");
+button_filter.addEventListener("click", filter);
+
+const button_split = document.getElementById("button_split");
+button_split.addEventListener("click", split);
+
+const button_join = document.getElementById("button_join");
+button_join.addEventListener("click", join);
+
+const button_some = document.getElementById("button_some");
+button_some.addEventListener("click", some);
+
+const button_reverse = document.getElementById("button_reverse");
+button_reverse.addEventListener("click", reverse);
+
+const button_spread = document.getElementById("button_spread");
+button_spread.addEventListener("click", spread);
+
+const button_reduce = document.getElementById("button_reduce");
+button_reduce.addEventListener("click", reduce);
+
 
 function push() {
     const i1 = eval(input1.value);
     const i2 = eval(input2.value);
     i1.push(i2);
     output.publish(i1);
+}
+
+function indexof() {
+    const i1 = eval(input1.value);
+    const i2 = eval(input2.value);
+    const res = i1.indexOf(i2);
+    output.publish(res);
+}   
+
+function includes() {
+    const i1 = eval(input1.value);
+    const i2 = eval(input2.value);
+    const res = i1.includes(i2);
+    output.publish(res);
+}
+function slice() {
+    const i1 = eval(input1.value);
+    const i2 = eval(input2.value);
+    const i3 = eval(input3.value);
+    const res = i1.slice(i2, i3);
+    output.publish(res);
+}
+
+function splice() {
+    const i1 = eval(input1.value);
+    const i2 = eval(input2.value);
+    const i3 = eval(input3.value);
+    const res = i1.splice(i2, i3);
+    output.publish(res);
+}
+function Of() {
+    const i1 = eval(input1.value);
+    let res = "";
+    for (const item of i1) {
+        res += item;
+    }
+    output.publish(res);
+}
+
+function Map() {
+    const i1 = eval(input1.value);
+    const i2 = eval(input2.value);             
+    const res = i1.map(value => `${i2} ${value}`);
+    output.publish(res);
+}
+
+function filter() {
+    const i1 = eval(input1.value);
+    const i2 = eval(input2.value);
+    const res = i1.filter(value => value > i2);
+    output.publish(res);                            //geht gerade nur mit Zahlen
+}
+
+function split() {                          //muss ich fragen
+    const i1 = eval(input1.value);
+    const i2 = input2; 
+    const res = i1.split(i2);
+    output.publish(res);
+}
+
+function join() {
+    const i1 = eval(input1.value);              
+    const i2 = input2.value;                //muss ich fragen
+    const res = i1.join(i2);
+    output.publish(res);
+}
+
+
+function some() {
+    const i1 = eval(input1.value);          
+    const i2 = eval(input2.value);
+    const res = i1.some(checkValue());
+   output.publish(res);
+                                            //musst ich auch noch fragen
+        
+    }
+    function checkValue(res)
+    {
+        const i2 = eval(input2.value);
+        return res > i2;
+    }
+   
+
+
+function reverse() {
+    const i1 = eval(input1.value);
+    const res = i1.reverse();           
+    output.publish(res);
+}
+
+function spread() {
+    const i1 = eval(input1.value);
+    const i2 = eval(input2.value);
+    const res = [...i1, ...i2];
+    output.publish(res);
+}
+
+function reduce() {                         //geht auch noch nicht
+    const i1 = eval(input1.value);
+    const i2 = eval(input2.value);  
+    const res = i1.reduce((accumulator, currentValue) => accumulator + currentValue, i2);
+    output.publish(res);
 }
